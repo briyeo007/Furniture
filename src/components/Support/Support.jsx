2dde7823a9a4c7edf0f733bad3f058342c730171
briyeo007 <brianuyeo@gmail.com>
2026-01-07 00:17:52 +0900

    update

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -7,7 +7,7 @@ export default function Support() {
         <div className="support__overlay">
           <h2 className="support__title">WE SUPPORT LA</h2>
           <p className="support__desc">
-            Over 4,000 mattresses donated to<br className="mobile-br" />fire victims and counting
+            Over 4,000 mattresses donated to<br className="mobile-br" /> fire victims and counting
           </p>
           <a href="#" className="support__btn">Learn More</a>
         </div>
